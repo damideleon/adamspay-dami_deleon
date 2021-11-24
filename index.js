dca9c27175e3f82c52ad8a08db45b6cbc9b67a74
dami_deleon <damideleon97@gmail.com>
2021-11-24 18:59:39 -0300

    fix path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path")
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
