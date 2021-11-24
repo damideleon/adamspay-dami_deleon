21b223b26060759eeccee29fd9e8d637e7cf712c
dami_deleon <damideleon97@gmail.com>
2021-11-23 22:58:33 -0300

    fix index

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', session, (req, res) =>{
+router.get('/', (req, res) =>{
     //response.json({info:'aplicación web funcionando'})
     res.render('index');
 })
