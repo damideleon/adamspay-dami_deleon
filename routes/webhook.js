f5a41a05c6c88a07c3fec2dede96b36e69f292de
Damián <damideleon97@gmail.com>
2021-11-26 07:46:41 -0300

    prueba de webhook

---
+++ routes/webhook.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+var md5 = require("md5");
+
+//var axios = require("axios")
+//var moment = require("moment")
+
+const apiSecret = "0e5810e847bb12580b";
+
+var db = require("../db")
+
+router.post("/", (req, res, next)=>{
+	console.log(req.body)
+	var notify = req.body.notify;
+
+	switch (notify.type){
+		case "debtStatus":
+			HMAC_esperado = md5("adams" + req.body + apiSecret);
+			HMAC_recibido = req.headers.HTTP_X_ADAMS_NOTIFY_HASH;
+			if(HMAC_esperado == HMAC_recibido){
+				res.sendStatus(200)
+			} else {
+				res.sendStatus(201)
+			}
+			break;
+		default:
+			res.sendStatus(201)
+	}
+})
+
+
+module.exports = router;
