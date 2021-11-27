241d0b28c0ad8f93b695a289df438a6d0d3f435c
dami_deleon <damideleon97@gmail.com>
2021-11-27 07:55:39 -0300

    fix webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -10,11 +10,15 @@ const apiSecret = "0e5810e847bb12580b";
 
 var db = require("../db")
 
-router.post("/", (req, res, next)=>{	
+router.post("/", (req, res, next)=>{
+	
+	console.log(req)
+
 	HMAC_esperado = md5("adams" + req + apiSecret);
 	HMAC_recibido = req.headers['x-adams-notify-hash'];
 	if(HMAC_esperado == HMAC_recibido){
 		console.log("OK hash")
+		res.sendStatus(200)
 	} else {
 		console.log('No ok Hash: ' + HMAC_esperado +' >><< ' + HMAC_recibido )
 		res.sendStatus(200)
