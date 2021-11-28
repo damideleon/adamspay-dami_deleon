a793ec71759c57a61c4cee0f83dec82d15bea635
dami_deleon <damideleon97@gmail.com>
2021-11-28 16:47:42 -0300

    fix webhook

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require("path")
-const bodyParser = require("bodyParser")
+const bodyParser = require("body-parser")
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
