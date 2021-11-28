32e750b1b674f3b35af518326be0e640ba0f2d92
dami_deleon <damideleon97@gmail.com>
2021-11-27 22:17:51 -0300

    pagina recargada

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const path = require("path")
 
 router.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../public/html/admin.html'));   
