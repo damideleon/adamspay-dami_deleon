1ea816476d1cd6905f8c16c641044f7d7051d890
dami_deleon <damideleon97@gmail.com>
2021-11-28 13:13:42 -0300

    fix db controller

--- routes/pagos.js
+++ routes/pagos.js
@@ -17,12 +17,8 @@ router.get('/', async (req, res) => {
         res.json(rs.rows);
     });
 }).post("/", async (req, res, next) => {
-    var detalle = JSON.parse(req.body.detalle);
-    console.log(typeof detalle)
-    console.log(detalle[0])
 
-    res.sendStatus(200)
-   /*
+  
     const { Pool } = require('pg')
     const pool = new Pool({
         connectionString: process.env.DATABASE_URL,
@@ -47,7 +43,7 @@ router.get('/', async (req, res) => {
                 var detalle = JSON.parse(req.body.detalle);
 
                 for (let i = 0; i < detalle.length; i++) {
-                    client.query(queryDetalle, [res.rows[0].venta_id, detalle[i].producto_cod, detalle[i].cantidad])
+                    client.query(queryDetalle, [res.rows[0].venta_id, detalle[i].producto_id, detalle[i].cantidad])
                 }
                 await client.query('COMMIT') //comit en la bd
                 //crear deuda en ADAMSPAY
@@ -98,6 +94,6 @@ router.get('/', async (req, res) => {
             }
         })().catch(e =>{
             console.error(e.message)
-        })*/
+        })
 });
 module.exports = router;
