17567afecc2127b7f27fd13bbb18b53ad8a7782d
dami_deleon <damideleon97@gmail.com>
2021-11-30 14:59:15 -0300

    fix checkout

--- routes/home.js
+++ routes/home.js
@@ -25,17 +25,19 @@ router.get('/', function(req, res, next) {
       console.error(e.message)
     }
     data.cliente = result.rows[0]
-  })
-
-  db.query("SELECT *, (c.cantidad * p.producto_precio) as total " + 
+    db.query("SELECT *, (c.cantidad * p.producto_precio) as total " + 
   "from ctrl_productos c JOIN producto p ON c.producto_id = p.producto_id  " +
   "where c.venta_id = $1;", [req.query.doc_id], (e, result)=>{
     if(e){
       console.error(e.message)
     }
     data.producto = result.rows[0]
-  })
     res.render("congrats", data);
+  })
+    
+  })
+
+  
 })
 
 
