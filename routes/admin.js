ac1a1a88a4711f26a36eb0544c44f4f1cbcaa9a2
dami_deleon <damideleon97@gmail.com>
2021-12-01 23:30:15 -0300

    admin page

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const db = require("../db");
 
 router.get('/', async (req, res, next) =>{
-    db.query("select v.*, cli.cliente_nombre from venta v join cliente cli on v.cliente_id = cli.cliente_id;", [], (err, result)=>{
+    db.query("select v.*, cli.cliente_nombre from venta v join cliente cli on v.cliente_id = cli.cliente_id order by venta_id desc;", [], (err, result)=>{
         res.render("admin", {data: result.rows})
     })
 })
